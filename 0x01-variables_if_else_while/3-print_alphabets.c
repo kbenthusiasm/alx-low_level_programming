ee02fa0c39576afc17d233081270752d2fe42700
kbenthusiasm <kbenthusiasm@gmail.com>
2023-06-19 05:09:42 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,8 +8,8 @@
  */
 int main(void)
 {
-        char low;
-        for(low = 'a'; low <= 'z'; low++)
+char low;
+	for(low = 'a'; low <= 'z'; low++)
        	putchar(low);
 	for(low = 'A'; low <= 'Z'; low++)
 	putchar(low);
