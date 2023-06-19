1d29f69c6eefbd1c5935f7b8eae5e54135cee3c7
kbenthusiasm <kbenthusiasm@gmail.com>
2023-06-19 07:04:30 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints lower case
+ * Return: 0. (Pass)
+ */
+
+int main(void)
+{
+	char low, e, q;
+	e = 'e';
+	q = 'q';
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+	if (low != e && low != q)
+	putchar(low);
+	}
+	putchar('\n');
+	return (0);
+}
