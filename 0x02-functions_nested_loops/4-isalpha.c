dea0a31cff0d33d53db54721d050808f4788d6e1
kbenthusiasm <kbenthusiasm@gmail.com>
2023-06-20 13:15:30 -0700

    4-isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,12 +2,11 @@
 
 /**
  * _isalpha - Checks for alphabetic character
- * @C! The character to be checked
+ * @c! The character to be checked
  * Return: 1 for alphabetic character or 0 for anything else
  */
 
 int _isalpha(int c)
-
 {
 	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
 	{
