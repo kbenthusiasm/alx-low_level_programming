6e469a7c8777d334877ea1ea525b1a93cc1a8e2e
Kibrom Legesse <kbenthusiasm@gmail.com>
2023-06-26 08:42:37 -0700

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *_strlen - Returns the length of a string.
- *@str: The string to get the length of 
- *Return: The length of @str.
+ * _strlen - Returns the length of a string.
+ * @str: The string to get the length of
+ * Return: The length of @str.
  */
 size_t _strlen(const char *str)
 {
