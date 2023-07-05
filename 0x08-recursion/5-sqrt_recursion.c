be89465d18da798cd64f87f164e9c6a69b9b8fd0
Kibrom Legesse <kbenthusiasm@gmail.com>
2023-07-05 11:44:59 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -22,12 +22,9 @@ int _sqrt_recursion(int n)
 int _sqrt(int n, int i)
 {
 	int sqrt = i * i;
-
 	if (sqrt > n)
 		return (-1);
-
 	if (sqrt == n)
 		return (i);
-
-	return (sqrt(n, i + 1));
+	return (_sqrt(n, i + 1));
 }
